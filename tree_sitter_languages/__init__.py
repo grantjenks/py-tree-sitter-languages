bc4c0bb9ad7096d42df4c70b06b2c2f12a7dc541
Grant Jenks <gjenks@linkedin.com>
2022-05-14 21:37:30 -0700

    Fix binary path

--- tree_sitter_languages/__init__.py
+++ tree_sitter_languages/__init__.py
@@ -1,11 +1,14 @@
 """Tree Sitter with Languages
 """
 
+import pathlib
+
 from tree_sitter import Language, Parser
 
 
 def get_parser(language):
-    language = Language('languages.so', language)
+    binary_path = str(pathlib.Path(__file__).parent / 'languages.so')
+    language = Language(binary_path, language)
     parser = Parser()
     parser.set_language(language)
     return parser

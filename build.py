1daae98a1798dba813d000929883b76f9af0c8ed
sansmoraxz <31374920+sansmoraxz@users.noreply.github.com>
2024-01-05 02:32:36 +0530

    Parellel build

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+from concurrent.futures import ThreadPoolExecutor
 from tree_sitter import Language
 
 
@@ -11,26 +12,36 @@ with open("repos.txt", "r") as file:
         clone_directory = os.path.join("vendor", url.rstrip("/").split("/")[-1])
         repos.append((url, commit, clone_directory))
 
+
+def clone_repo(url, commit, clone_directory):
+    print()
+    print(f"{sys.argv[0]}: Cloning: {url} (commit {commit}) --> {clone_directory}")
+    print()
+
+    if os.path.exists(clone_directory):
+        return
+
+    # https://serverfault.com/a/713065
+    os.mkdir(clone_directory)
+    subprocess.check_call(["git", "init"], cwd=clone_directory)
+    subprocess.check_call(["git", "remote", "add", "origin", url], cwd=clone_directory)
+    subprocess.check_call(["git", "fetch", "--depth=1", "origin", commit], cwd=clone_directory)
+    subprocess.check_call(["git", "checkout", commit], cwd=clone_directory)
+
 # During the build, this script runs several times, and only needs to download
 # repositories on first time.
 if os.path.isdir("vendor") and len(os.listdir("vendor")) == len(repos):
     print(f"{sys.argv[0]}: Language repositories have been cloned already.")
 else:
     os.makedirs("vendor", exist_ok=True)
-    for url, commit, clone_directory in repos:
-        print()
-        print(f"{sys.argv[0]}: Cloning: {url} (commit {commit}) --> {clone_directory}")
-        print()
+    futures = []
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        for url, commit, clone_directory in repos:
+            futures.append(executor.submit(clone_repo, url, commit, clone_directory))
+    # Wait for all the clones to finish.
+    for future in futures:
+        future.result()
 
-        if os.path.exists(clone_directory):
-            continue
-
-        # https://serverfault.com/a/713065
-        os.mkdir(clone_directory)
-        subprocess.check_call(["git", "init"], cwd=clone_directory)
-        subprocess.check_call(["git", "remote", "add", "origin", url], cwd=clone_directory)
-        subprocess.check_call(["git", "fetch", "--depth=1", "origin", commit], cwd=clone_directory)
-        subprocess.check_call(["git", "checkout", commit], cwd=clone_directory)
 
 print()
 

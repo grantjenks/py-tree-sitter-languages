42f4baffec92848be4937b0cc52b2872201fe322
Grant Jenks <grant.jenks@gmail.com>
2024-02-03 22:12:50 -0800

    Bump version to 1.10.2

--- tree_sitter_languages/__init__.py
+++ tree_sitter_languages/__init__.py
@@ -3,7 +3,7 @@
 
 from .core import get_language, get_parser
 
-__version__ = '1.10.1'
+__version__ = '1.10.2'
 __title__ = 'tree_sitter_languages'
 __author__ = 'Grant Jenks'
 __license__ = 'Apache 2.0'
